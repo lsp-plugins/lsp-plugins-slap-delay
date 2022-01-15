4f3e827a14b81317fe47ffa24ca588e57f9e4ce1
sadko4u <sadko4u@gmail.com>
2022-01-16 01:43:46 +0300

    Updated build scripts

--- plugins.mk
+++ plugins.mk
@@ -18,13 +18,6 @@
 # along with lsp-plugins-slap-delay.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-# Package version
-ARTIFACT_ID                 = LSP_PLUGINS_SLAP_DELAY
-ARTIFACT_NAME               = lsp-plugins-slap-delay
-ARTIFACT_TYPE               = plug
-ARTIFACT_DESC               = LSP Slap-back Delay Plugin Series
-ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.2-devel
-
-
-
+# Variables that describe dependencies
+PLUGIN_DEPENDENCIES     = LSP_PLUGINS_SLAP_DELAY
+PLUGIN_PACKAGE_NAME     = lsp-plugins
