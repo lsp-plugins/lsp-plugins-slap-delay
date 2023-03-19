326fa2811802a738ee6878dbddbb17a8d5fd7296
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-03-19 15:24:44 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -400,6 +400,7 @@ $(LSP_PLUGINS_SLAP_DELAY_BIN)/main/ui/slap_delay.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPlayListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ProxyPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
